2d437d2222225fec123018ba8585946e2bfea6f5
Gabin BLOQUET <gabin.blqt@gmail.com>
2024-07-08 22:31:58 +0200

    chore: database and first table

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test-containers sandbox
+
+This project aims to practice [Testcontainers library](https://testcontainers.com/).
+
---
+++ src/main/resources/db/migration/V1__create_action.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "ACTIONS"
+(
+    title          varchar(50)              NOT NULL PRIMARY KEY,
+    description    varchar(255)             NOT NULL
+);
