92962776c6a748bc849d96c74fe7aae4ce0cb83f
Gabin BLOQUET <gabin.blqt@gmail.com>
2024-07-08 21:35:57 +0200

    chore: init project :tada:

---
+++ src/main/java/io/github/test_containers_sandbox/TestContainersSandboxApplication.java
@@ -0,0 +1,13 @@
+package io.github.test_containers_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersSandboxApplication.class, args);
+	}
+
+}
